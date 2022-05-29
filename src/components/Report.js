d2637c71b45dbbee6540d2bd7f84168b538330f8
maxwell142857 <1587912667@qq.com>
2022-05-29 22:45:43 +0800

    报告内容整合后端部分

--- src/components/Report.js
+++ src/components/Report.js
@@ -3,7 +3,15 @@ import React from "react";
 import trace from '../static/trace.mp4'
 import od from '../static/OD.mp4'
 import agg from '../static/aggregation.png'
-
+import daily_travel from '../static/daily_travel.png'
+import number_of_passenger from '../static/number_of_passenger.png'
+import passenger_time from '../static/passenger_time.png'
+import speed_with from '../static/speed_with.png'
+import speed_without from '../static/speed_without.png'
+import district_people from '../static/district_people.png'
+import district_inon from '../static/district_inon.png'
+import district from '../static/district.png'
+import map from '../static/map.png'
 class Report extends React.Component {
 
 
@@ -24,8 +32,27 @@ class Report extends React.Component {
                     <video  src={od} controls></video>
                     <h2 className=" font-bold text-2xl py-1">四、轨迹可视化</h2>
                     <video  src={trace} controls></video>
+                    <h1 className=" font-bold text-4xl py-2">后端结果分析</h1>
+                    <h2 className=" font-bold text-2xl py-1">一、上车人数随时间变化的折线图</h2>
+                    <img src={passenger_time}></img>
+                    <h2 className=" font-bold text-2xl py-1">二、出租车一天行驶距离分布</h2>
+                    <img src={daily_travel}></img>
+                    <h2 className=" font-bold text-2xl py-1">三、出租车一天载客人数分布</h2>
+                    <img src={number_of_passenger}></img>
+                    <h2 className=" font-bold text-2xl py-1">四、出租车载客速度分布</h2>
+                    <img src={speed_with}></img>
+                    <h2 className=" font-bold text-2xl py-1">五、出租车空载速度分布</h2>
+                    <img src={speed_without}></img>
+                    <div className="flex flex-row">
+                    <h2 className=" font-bold text-2xl py-1">六、各区出行人数统计  </h2>
+                    <div className="ml-20  ">
+                        <h2 className="font-bold text-2xl py-1">七、各区人员流动情况  </h2>
+                        <p>District 为上车地点， 后面几列为对应下车地点所在的区</p>
+                    </div>
+                    </div>
+                    <img src={district}></img>
+                    <img className=" h-96 " src={map}></img>
                     
-
 
                 </div>
 			</div>
